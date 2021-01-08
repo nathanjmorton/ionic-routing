c03ae73c61be90a1645f33f29a58ddbae1a6b134
Nathan Morton <nathan.j.morton@gmail.com>
2021-01-08 00:04:32 -0500

    this

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
-import { IonApp } from '@ionic/react';
+import { IonApp, IonLoading } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { AuthContext } from './auth';
 import AppTabs from './AppTabs';
 import LoginPage from './pages/LoginPage';
 import PageNotFound from './pages/PageNotFound';
+import { auth } from './firebase';
+import RegisterPage from './pages/RegisterPage';
 
 const App: React.FC = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  console.log(`rendering App with loggedIn=${loggedIn}`);
+  const [authState, setAuthState] = useState({
+    loading: true,
+    loggedIn: false,
+  });
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setAuthState({ loading: false, loggedIn: Boolean(user) });
+    });
+  }, []);
+  console.log(`rendering app with authState: `, authState);
+  if (authState.loading) {
+    return <IonLoading isOpen />;
+  }
   return (
     <IonApp>
-      <AuthContext.Provider value={{ loggedIn }}>
+      <AuthContext.Provider value={{ loggedIn: authState.loggedIn }}>
         <IonReactRouter>
           <Switch>
             <Route exact path='/login'>
-              <LoginPage onLogin={() => setLoggedIn(true)} />
+              <LoginPage />
+            </Route>
+            <Route exact path='/register'>
+              <RegisterPage />
             </Route>
             <Route path='/my'>
               <AppTabs />
-            </Route>
-            <Route exact path='/page-not-found'>
-              <PageNotFound />
             </Route>
             <Redirect exact path='/' to='/my/entries' />
             <Route>
---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCp0t5eFot6mgG1_u_ZZHCmAvmSNortXYQ',
+  authDomain: 'daily-moments-25049.firebaseapp.com',
+  databaseURL: 'https://daily-moments-25049-default-rtdb.firebaseio.com',
+  projectId: 'daily-moments-25049',
+  storageBucket: 'daily-moments-25049.appspot.com',
+  messagingSenderId: '841574956051',
+  appId: '1:841574956051:web:4c2281335553f48a9d93e6',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+export const auth = app.auth();
